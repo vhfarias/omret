893e01f338c11e6ed2e762825f54a1a51c1bb848
Victor Hugo Garcia de Farias <vhfarias_1@hotmail.com>
2022-04-06 22:46:06 -0300

    restructured folders

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ let app = express();
 const PORT = 4000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 })
+
 app.get('/css/:name', (req, res) => {
-  res.sendFile(__dirname + '/css/' + req.params.name);
+  res.sendFile(__dirname + '/public/css/' + req.params.name);
 })
 
 app.get('/js/:name', (req, res) => {
-  res.sendFile(__dirname + '/js/' + req.params.name);
+  res.sendFile(__dirname + '/public/js/' + req.params.name);
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
