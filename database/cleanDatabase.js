b9a023f015cd25b005da5e124d5fa75ed376e6fd
Victor Hugo Garcia de Farias <vhfarias_1@hotmail.com>
2022-04-20 13:47:48 -0300

    further improved database filtering

--- database/cleanDatabase.js
+++ database/cleanDatabase.js
@@ -1,11 +1,24 @@
 let prompt = require('prompt-sync')();
 let { readFileSync, writeFileSync } = require('fs');
 
-let input = Array.from(Object.entries(JSON.parse(readFileSync(__dirname + '/words.json'))));
+//importa o objeto do arquivo
+let input = JSON.parse(readFileSync(__dirname + '/words.json'));
 
-let output = input.filter((el, i, arr) => {
-  let filter = prompt(`(${i + 1}/${arr.length}) Manter ${el[1]}? (y/n): `)
-  return filter.toLowerCase() === 'y'
+//converte o objeto em um array para facilidade de iteração
+let data = Array.from(Object.entries(input));
+
+//ordem alfabética
+//data = data.sort((a, b) => a[0].localeCompare(b[0], 'pt-BR'));
+
+//embaralhar palavras
+/* for (let i = 0; i < 1000; i++) {
+  data = data.sort((a, b) => Math.random() - 0.5);
+} */
+
+// confirma palavra a palavra
+let output = data.filter((el, i, arr) => {
+  let filter = prompt(`(${i + 1}/${arr.length}) Manter ${el[1]}? (y/n): `);
+  return filter.toLowerCase() === 'y';
 })
 
 writeFileSync(__dirname + '/words.json', JSON.stringify(Object.fromEntries(output), null, ' '));
